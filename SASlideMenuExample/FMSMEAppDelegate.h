30ae44ddacd33e46da8352744f8a20fce1860792
Frank Münchow <frank.muenchow@hotmail.de>
2013-04-20 16:32:03 +0200

    Initial Commit

---
+++ SASlideMenuExample/FMSMEAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  FMSMEAppDelegate.h
+//  SASlideMenuExample
+//
+//  Created by Frank Muenchow on 20.04.13.
+//  Copyright (c) 2013 Frank Muenchow. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FMSMEAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SASlideMenuExample/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SASlideMenuExample
+//
+//  Created by Frank Muenchow on 20.04.13.
+//  Copyright (c) 2013 Frank Muenchow. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "FMSMEAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([FMSMEAppDelegate class]));
+    }
+}
